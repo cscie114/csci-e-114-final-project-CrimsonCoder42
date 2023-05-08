d8fd50d7b9546db054c78d4b62dc3e8691235aee
CrimsonCoder42 <nostro37@gmail.com>
2023-05-08 10:56:07 -0400

    corrected

---
+++ next_park_weather_proj/pages/contact.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import MainLayout from '../components/layout/layout';
+
+const ContactPage = () => {
+  return ( <
+    MainLayout >
+    <
+    div className = "container" >
+    <
+    h1 > Contact Us < /h1> <
+    p > If you have any questions or comments, please don & apos; t hesitate to contact us: < /p> <
+    ul >
+    <
+    li > Name: Devin Anderson < /li> <
+    li > Organization: Harvard University < /li> <
+    li > Email: nostro37 @gmail.com < /li> <
+    li > Phone: 203 - 715 - 4454 < /li> < /
+    ul > <
+    /div> < /
+    MainLayout >
+  );
+};
+
+export default ContactPage;
